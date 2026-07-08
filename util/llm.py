f9cb2f1cea06b98dbfbe715e6e06f5669bcdb151
DaviKM <davikmach@gmail.com>
2026-07-08 11:20:29 -0300

    atualização do modelo da ia e criação da função de retorno do embedding

--- util/llm.py
+++ util/llm.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
 def googleLLM():
-    return GoogleGenerativeAI(model='gemini-3.5-flash', google_api_key=API_KEY)
+    return GoogleGenerativeAI(model='gemini-3.1-flash-lite', google_api_key=API_KEY)
 
+def googleEmbedding(model = 'gemini-embedding-001'):
+    return GoogleGenerativeAIEmbeddings(model=model)
