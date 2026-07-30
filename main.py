dfaf684ad283ec23d5871d0e76b85b8a0aefbe29
DaviKM <davikmach@gmail.com>
2026-07-30 00:13:03 -0300

    adiciona funcionalidade de upload de arquivo no rag_controller

--- main.py
+++ main.py
@@ -11,4 +11,3 @@ if __name__ == "__main__":
                 #host="0.0.0.0",
                 port=6060,
                 reload=True)
-    print("App inicializado")
--- src/controllers/rag_controller.py
+++ src/controllers/rag_controller.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, File
 from src.service.rag_service import treinarArquivo
+import os
 
 router = APIRouter(prefix='/treinar', tags=['Treinar', 'IA', 'PDF'])
 
 @router.post('')
-def treinarArquivo(file: UploadFile):
-    path = file.filename
+async def treinarArquivo(arquivo: UploadFile = File()):
+    try:
+        path = os.path.join("src", "controllers", "uploads", arquivo.filename)
+        with open(path, 'wb') as f:
+            arquivo = await arquivo.read()
+            f.write(arquivo)
+        return 'Upload your file'
+    except Exception as e:
+        return e
     response = treinarArquivo(path)
     return response
